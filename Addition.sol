547458ac39e0597fb7ddeeb4193943c319be54fc
Poorna Shri <poornashri2703@gmail.com>
2023-09-01 10:52:57 +0530

    addition

---
+++ Addition.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract addition{
+
+int a = 50;
+int b = 50;
+
+function add()public view  returns(int){
+    int result = a+b;
+     return result ;
+
+}
+
+}
